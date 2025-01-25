ede03704287fa3a377e2eba1386dddff32525009
rocketcamel <luca_lise@icloud.com>
2025-01-25 21:41:09 +0000

    fix: default editor set to nvim

--- nix/users/luca/zsh.nix
+++ nix/users/luca/zsh.nix
@@ -3,6 +3,7 @@
   enableCompletion = true;
   autosuggestion.enable = true;
   envExtra = ''
+    EDITOR=nvim
     . "$HOME/.rokit/env"
     eval "$(oh-my-posh init zsh -c ~/.config/ohmyposh/zen.toml)"
   '';
