cf5e3e3ae728217d3289805cd9f46db6324b02bf
rocketcamel <luca_lise@icloud.com>
2025-02-04 22:36:59 +0000

    feat: fugitive!

--- .config/nvim/lua/rocketcamel/remap.lua
+++ .config/nvim/lua/rocketcamel/remap.lua
@@ -5,3 +5,12 @@ vim.keymap.set("i", "<C-c>", "<Esc>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = false })
 vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true, silent = true })
 vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "Git commit" })
+vim.keymap.set("n", "<leader>ga", function()
+	vim.ui.input({ prompt = "Git add: " }, function(input)
+		if input then
+			vim.cmd("Git add " .. input)
+		end
+	end)
+end)
