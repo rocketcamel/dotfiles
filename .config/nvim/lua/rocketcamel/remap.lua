ea6597b7c4477e34c1fabfcf7910c2b7e6e07a2c
rocketcamel <luca_lise@icloud.com>
2025-05-31 15:51:49 -0700

    feat: add nvim-tree

--- .config/nvim/lua/rocketcamel/remap.lua
+++ .config/nvim/lua/rocketcamel/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeOpen)
 vim.keymap.set("n", "<leader>x", vim.cmd.q)
 vim.keymap.set("i", "<C-c>", "<Esc>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = false })
