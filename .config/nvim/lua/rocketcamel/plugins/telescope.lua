ef6781cec1c11d9966feb6e176a770de6e1e6d20
rocketcamel <luca_lise@icloud.com>
2025-05-30 22:53:35 -0700

    feat: add telescope tabs

--- .config/nvim/lua/rocketcamel/plugins/telescope.lua
+++ .config/nvim/lua/rocketcamel/plugins/telescope.lua
@@ -1,12 +1,20 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"LukasPietzschmann/telescope-tabs",
+		},
 		config = function()
+			local telescope = require("telescope")
+			local telescope_tabs = require("telescope-tabs")
 			local builtin = require("telescope.builtin")
+			telescope.load_extension("telescope-tabs")
+			telescope_tabs.setup()
 			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 			vim.keymap.set("n", "<leader>pb", builtin.buffers)
 			vim.keymap.set("n", "<leader>ps", builtin.live_grep)
+			vim.keymap.set("n", "<leader>pt", telescope_tabs.list_tabs)
 		end,
 	},
 }
