eebce1c9edbe6c11d8a095ad1bdfdd2db6cc5145
rocketcamel <luca_lise@icloud.com>
2025-07-02 17:31:43 -0700

    fix: hypridle disable locking screen automatically

--- nix/modules/desktop.nix
+++ nix/modules/desktop.nix
@@ -106,10 +106,6 @@
               on-resume = "brightnessctl -r";
             }
             {
-              timeout = 300;
-              on-timeout = "loginctl lock-session";
-            }
-            {
               timeout = 330;
               on-timeout = "hyprctl dispatch dpms off";
               on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
