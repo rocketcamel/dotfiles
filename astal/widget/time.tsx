01cae1825d9469fddd0979c48053e5675f40679a
rocketcamel <luca_lise@icloud.com>
2025-09-16 19:23:18 -0700

    fix: add date and day to time

--- astal/widget/time.tsx
+++ astal/widget/time.tsx
@@ -3,7 +3,7 @@ import { bind, GLib, Variable } from "astal"
 let current_time = Variable(GLib.DateTime.new_now_local().format("%H:%M"))
 GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, () => {
   const now = GLib.DateTime.new_now_local()
-  current_time.set(now.format("%H:%M"))
+  current_time.set(now.format("%b %e (%a) %H:%M"))
   return true
 })
 export default function Time() {
