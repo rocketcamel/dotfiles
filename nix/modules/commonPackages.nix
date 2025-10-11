aae6b21116f81ae2121161e6e43e88891ee4837c
rocketcamel <luca_lise@icloud.com>
2025-10-10 21:17:46 -0700

    feat: add doppler, update flake

--- nix/modules/commonPackages.nix
+++ nix/modules/commonPackages.nix
@@ -64,7 +64,11 @@
       python3
       jdt-language-server
       gradle
+<<<<<<< Updated upstream
       cmake
+=======
+      doppler
+>>>>>>> Stashed changes
     ];
     programs.nix-ld.enable = lib.mkDefault true;
     programs.zsh.enable = lib.mkDefault true;
