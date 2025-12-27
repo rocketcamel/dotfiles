037036a6842bfeddba35b55bb075551a911c32ae
lucalise <luca_lise@icloud.com>
2025-12-27 00:28:14 -0800

    fix(homelab): add restore-job generation

---
+++ nix/homelab/kustomize/generate-restore-job.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <server_name> <backup_file>" >&2
+  exit 1
+fi
+
+SERVER_NAME="$1"
+BACKUP_FILE="$2"
+
+kubectl scale deployment minecraft-$SERVER_NAME --replicas 0
+
+sed -e "s/{{SERVER_NAME}}/$SERVER_NAME/g" \
+    -e "s/{{BACKUP_FILE}}/$BACKUP_FILE/g" \
+    restore-job.yaml | kubectl apply -f -
